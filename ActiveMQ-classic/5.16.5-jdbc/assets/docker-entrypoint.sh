c6551addd96008b0c64f35ec7ca84476f731710d
eimantasLinartas <eimantas.linartas@thehutgroup.com>
2023-01-19 13:25:30 +0200

    msg

--- ActiveMQ-classic/5.16.5-jdbc/assets/docker-entrypoint.sh
+++ ActiveMQ-classic/5.16.5-jdbc/assets/docker-entrypoint.sh
@@ -9,5 +9,5 @@ sed -i "s/CREATE_DB_TABLES/${CREATE_DB_TABLES}/" /opt/apache-activemq-5.16.5/con
 sed -i 's/127.0.0.1/0.0.0.0/g' $ACTIVEMQ_WORKDIR/apache-activemq-5.16.5/conf/jetty.xml
 # @ToDO need to test this auth property line as it repeats in few lines
 sed -i 's/<property name="authenticate" value="true" \/\>/<property name="authenticate" value="true" \/\>/g' $ACTIVEMQ_WORKDIR/apache-activemq-5.16.5/conf/jetty-realm.properties
-sed -i "s/admin: admin, admin/admin: $ACTIVEMQ_ADMIN_PASS, admin/g" $ACTIVEMQ_WORKDIR/apache-activemq-5.16.5/conf/jetty-realm.properties
+sed -i "s/admin: admin, admin/admin: ${ACTIVEMQ_ADMIN_PASS}, admin/g" $ACTIVEMQ_WORKDIR/apache-activemq-5.16.5/conf/jetty-realm.properties
 bin/activemq console
