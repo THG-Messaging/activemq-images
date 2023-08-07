f7fa5ce0187f70cfbb766137e1870b0e9bae94c3
Eimantas Linartas <linartase@thehutgroup.com>
2023-08-07 14:22:41 +0300

    transport connectors service and some fix to env

--- ActiveMQ-classic/5.16.5-jdbc/assets/docker-entrypoint.sh
+++ ActiveMQ-classic/5.16.5-jdbc/assets/docker-entrypoint.sh
@@ -7,7 +7,11 @@ sed -i "s/DB_HOST/${DB_HOST}/" /opt/apache-activemq-5.16.5/conf/activemq.xml
 sed -i "s/DB_USERNAME/${DB_USERNAME}/" /opt/apache-activemq-5.16.5/conf/activemq.xml
 sed -i "s/DB_PASSWORD/${DB_PASSWORD}/" /opt/apache-activemq-5.16.5/conf/activemq.xml
 sed -i "s/DB_NAME/${DB_NAME}/" /opt/apache-activemq-5.16.5/conf/activemq.xml
+if [ "$OPENWIRE_ENABLED" = true ] ; then
 sed -i "s/CREATE_DB_TABLES/${CREATE_DB_TABLES}/" /opt/apache-activemq-5.16.5/conf/activemq.xml
+else
+sed -i "s/CREATE_DB_TABLES/false/" /opt/apache-activemq-5.16.5/conf/activemq.xml
+fi
 sed -i "s/USE_JMX/${USE_JMX}/" /opt/apache-activemq-5.16.5/conf/activemq.xml
 sed -i 's/127.0.0.1/0.0.0.0/g' $ACTIVEMQ_WORKDIR/apache-activemq-5.16.5/conf/jetty.xml
 sed -i "s/MONITOR_ROLE_PASS/${MONITOR_ROLE_PASS}/" $ACTIVEMQ_WORKDIR/apache-activemq-5.16.5/conf/jmx.password
@@ -28,8 +32,6 @@ fi
 # @ToDO need to test this auth property line as it repeats in few lines
 sed -i 's/<property name="authenticate" value="true" \/\>/<property name="authenticate" value="true" \/\>/g' $ACTIVEMQ_WORKDIR/apache-activemq-5.16.5/conf/jetty-realm.properties
 sed -i "s/admin: admin, admin/admin: ${ACTIVEMQ_ADMIN_PASS}, admin/g" $ACTIVEMQ_WORKDIR/apache-activemq-5.16.5/conf/jetty-realm.properties
-echo labas
-echo $MONITOR_ROLE_PASS
 # @ToDo make prom exporter as optional
 if [ "$METRICS_ENABLED" = true ] ; then
 java -jar ${CONTAINER_METRICS}/jmx_prometheus_httpserver-0.17.2.jar 12345 ${CONTAINER_METRICS}/config.yaml &
