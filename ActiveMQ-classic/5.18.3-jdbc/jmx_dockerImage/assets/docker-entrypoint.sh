b78af470feef302186f8b42ca79d9968be7148f8
Karolis Dzekunskas <karolis.dzekunskas@zenitech.co.uk>
2024-06-13 16:29:40 +0300

    add service_port variable

--- ActiveMQ-classic/5.18.3-jdbc/jmx_dockerImage/assets/docker-entrypoint.sh
+++ ActiveMQ-classic/5.18.3-jdbc/jmx_dockerImage/assets/docker-entrypoint.sh
@@ -9,4 +9,4 @@ sed -i "s/HOSTNAME/${HOSTNAME}/" ${METRICS_WORKDIR}/config.yaml
 sed -i "s/PORT/${PORT}/" ${METRICS_WORKDIR}/config.yaml
 sed -i "s/MONITOR_ROLE_PASS/${MONITOR_ROLE_PASS}/" ${METRICS_WORKDIR}/config.yaml
 
-java -jar ${METRICS_WORKDIR}/jmx_prometheus_httpserver-${EXPORTER_VERSION}.jar ${PORT} ${METRICS_WORKDIR}/config.yaml
+java -jar ${METRICS_WORKDIR}/jmx_prometheus_httpserver-${EXPORTER_VERSION}.jar ${SERVICE_PORT} ${METRICS_WORKDIR}/config.yaml
